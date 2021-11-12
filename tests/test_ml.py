068c2eceae7cf3543a085acc54156aae520d4bc8
Bharath kumar <34125323+allambharath@users.noreply.github.com>
2021-11-12 17:09:16 +0530

    Update test_ml.py

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -7,9 +7,9 @@ def test_percentage():
     assert ml.func2() < 50
 
 def test_addition():
-    assert ml.add(4,5) == 10
-    assert ml.add(5, 7) == 34
-    assert ml.add(4, 5) == 10
+    assert ml.add(4,5) == 9
+    assert ml.add(5, 7) == 12
+    assert ml.add(4, 5) == 9
 
 def test_division():
     assert ml.product(4) == 8
